9ff9282a47d919b95a0ddf6f876786c212d0eb5d
Thomas Roth <rothen_90@msn.com>
2020-05-14 21:00:55 +0200

    added html

--- src/main/java/com/zetcode/GetValueRightNow.java
+++ src/main/java/com/zetcode/GetValueRightNow.java
@@ -15,12 +15,34 @@ public class GetValueRightNow extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Database db = new Database();
-        response.setContentType("text/plain;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
 
         var out = response.getOutputStream();
+        out.println("<html>\n" +
+                "<head>\n" +
+                "</head>\n" +
+                "<body>\n" );
+        out.println("<h4>  Data from SQL  </h4> ");
+        out.println("<table style=\"width:30%\">\n" +
+                "<tr>\n" +
+                "<th>Temperature</th>\n" +
+                "<th>Humidity</th>\n" +
+                "<th>Date</th>\n" +
+                "<th colspan=\"4\">Time</th>\n" +
+                "</tr>\n");
 
         for (Sensor s: db.getLatest(1)){
-            out.println(s.getId()+" "+s.getTemperature()+" "+s.getHumidity()+" "+s.getDate()+" "+s.getTime());
+
+            out.println("<tr>" +
+                    "<td>" +  s.getTemperature() +" °C</td>\n" +
+                    "<td>" +  s.getHumidity() + " %</td>\n" +
+                    "<td>" +  s.getDate() + " </td>\n" +
+                    "<td>" + s.getTime() + " </td>\n" );
+
+            out.println("</tr>\n");
         }
+        out.println("</table>\n" +
+                "</body>\n" +
+                "</html>");
     }
 }
