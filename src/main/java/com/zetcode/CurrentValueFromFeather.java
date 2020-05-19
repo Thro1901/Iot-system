00605ec606cd523553b5262b5756e48c62fe8d20
Thomas Roth <rothen_90@msn.com>
2020-05-19 09:44:09 +0200

    Current value working

--- src/main/java/com/zetcode/CurrentValueFromFeather.java
+++ src/main/java/com/zetcode/CurrentValueFromFeather.java
@@ -9,22 +9,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Date;
 
 @WebServlet(name = "currentvalue", urlPatterns = {"/Currentvalue"})
 public class CurrentValueFromFeather extends HttpServlet {
 
-    String timeNow = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+    String timeNow;
     String temp;
     String hum;
-    String url = "http://5.150.211.190/readTemperature";
+    String url = "http://5.150.211.190/getValues";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     response.setContentType("text/html;charset=UTF-8");
     var out = response.getOutputStream();
-
 
         out.println("<!DOCTYPE html>\n" +
                 "<html>\n" +
@@ -35,41 +34,36 @@ public class CurrentValueFromFeather extends HttpServlet {
                 "<input type=\"submit\" value=\"Go back\" />\n" +
                 "</form>");
 
-     URL oracle = new URL(url);
+    URL oracle = new URL(url);
 
     BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
 
         out.println("<h4>  Current value from Feather  </h4> ");
+
         out.println("<table border=\"1\" style=\"width:30%\">\n" +
                 "<tr>\n" +
                 "<th>Temperature</th>\n" +
                 "<th>Humidity</th>\n" +
                 "<th>Date</th>\n" +
                 "</tr>\n");
-        while ((temp = in.readLine()) != null){
+        temp = in.readLine();
+        hum = in.readLine();
+        timeNow = in.readLine();
+
+        long unix_seconds = Long.parseLong(timeNow);
+        Date date = new Date(unix_seconds * 1000);
+        SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        timeNow = jdf.format(date);
 
             out.println("<tr>" +
-                    "<td>" + temp + " °C</td>\n");
-
-        }
-
-        String url1 = "http://5.150.211.190/readHumidity";
-        URL oracle1 = new URL(url1);
-
-        in = new BufferedReader(new InputStreamReader(oracle1.openStream()));
-
-        while ((hum = in.readLine()) != null){
-
-            out.println( "<td>" + hum + " %</td>\n" +
+                    "<td>" + temp + " °C</td>\n" +
+                    "<td>" + hum + " %</td>\n" +
                     "<td>" + timeNow + " </td>\n");
 
-        }
         out.println("</tr>\n");
         out.println("</table>");
         out.println("</body>\n" +
                 "</html>");
-
-
 
         in.close();
 
